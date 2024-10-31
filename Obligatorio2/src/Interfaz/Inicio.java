6ffd2a6769b3ba939f57875fac19053cc5e6a712
juanpemanya <167726582+juanpemanya@users.noreply.github.com>
2024-10-31 13:19:42 -0300

    mensaje de inicio

--- Obligatorio2/src/Interfaz/Inicio.java
+++ Obligatorio2/src/Interfaz/Inicio.java
@@ -2,6 +2,7 @@
 package Interfaz;
 
 import Dominio.*;
+import java.util.*;
 
 /*
 @author Juan Pedro Longo (329112)
@@ -12,9 +13,35 @@ public class Inicio {
     
     public static void main(String[] args) {
         // TODO code application logic here
-        Sistema sistema=new Sistema();
-        menuPrincipal vent=new menuPrincipal(sistema);
-        vent.setVisible(true);
+        msjInicio();
+        //Sistema sistema=new Sistema();
+        //menuPrincipal vent=new menuPrincipal(sistema);
+        //vent.setVisible(true);  
     }
     
+    public static void msjInicio(){
+        Scanner in = new Scanner (System.in);
+        System.out.println("Ingrese una opcion");
+        System.out.println("1- Utilizar el sistema con uno nuevo");
+        System.out.println("2- Utilizar el sistema de la ultima ejecucucion");
+        int op;
+        try {
+            op = Integer.parseInt(in.next());
+        } catch (Exception e) {
+            op = 99;
+        }
+        switch (op) {
+            case 1:
+                //se utiliza un sistema nuevo
+                Sistema sistema=new Sistema();
+                menuPrincipal vent=new menuPrincipal(sistema);
+                vent.setVisible(true);   
+            break;
+            case 2:
+                    
+            
+            default:
+                System.out.println("Opcion Incorrecta");
+        }
+    }
 }
